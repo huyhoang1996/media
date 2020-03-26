4c67893217b643c060b385d84b75f5ef56d118e2
huyhoang1996ha <huyhoang1996ha@gmail.com>
2020-03-26 11:24:32 +0700

    Log local

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"context"
 	"crypto/rand"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"mime"
 	"net/http"
 	"os"
@@ -12,6 +12,8 @@ import (
 	"time"
 
 	"github.com/go-pg/pg"
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
 )
 
 // func handler(w http.ResponseWriter, r *http.Request) {
@@ -46,7 +48,11 @@ import (
 // }
 
 const maxUploadSize = 2 * 1024 * 1024 // 2 mb
-const uploadPath = "./media"
+const uploadPath = "./mediafile"
+
+type msgError interface {
+	Error() string
+}
 
 func renderError(w http.ResponseWriter, message string, statusCode int) {
 	w.WriteHeader(http.StatusBadRequest)
@@ -59,26 +65,29 @@ func randToken(len int) string {
 	return fmt.Sprintf("%x", b)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) error {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 	// validate file size
 	r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
 	if err := r.ParseMultipartForm(maxUploadSize); err != nil {
 		renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
-		return
+		return err
 	}
 
 	// parse and validate file and post parameters
-	file, _, err := r.FormFile("file")
+	file, multipartFileHeader, err := r.FormFile("file")
 	if err != nil {
 		renderError(w, "INVALID_FILE", http.StatusBadRequest)
-		return
+		return err
 	}
 	defer file.Close()
+	fmt.Println("=== multipartFileHeader:: ", multipartFileHeader.Filename)
+
 	fileBytes, err := ioutil.ReadAll(file)
+	// fmt.Println("=== fileBytes:: ", string(fileBytes))
 	if err != nil {
 		renderError(w, "INVALID_FILE", http.StatusBadRequest)
-		return
+		return err
 	}
 
 	// check file type, detectcontenttype only needs the first 512 bytes
@@ -90,7 +99,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		break
 	default:
 		renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
-		return
+		return err
 	}
 	fileName := randToken(12)
 	fileEndings, err := mime.ExtensionsByType(detectedFileType)
@@ -99,7 +108,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		renderError(w, "CANT_READ_FILE_TYPE", http.StatusInternalServerError)
-		return
+		return err
 	}
 	newPath := filepath.Join(uploadPath, fileName+fileEndings[0])
 	fmt.Printf("FileType: %s, File: %s\n", detectedFileType, newPath)
@@ -107,20 +116,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// write file
 	newFile, err := os.Create(newPath)
 	if err != nil {
-		renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
-		return
+		renderError(w, "CANT_WRITE_FOLDER", http.StatusInternalServerError)
+		return err
 	}
 	defer newFile.Close() // idempotent, okay to call twice
 	if _, err := newFile.Write(fileBytes); err != nil || newFile.Close() != nil {
 		renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
-		return
+		return err
 	}
 
 	db := pg.Connect(&pg.Options{
 		User:                  "postgres",
 		Password:              "huyhoang@123",
 		Database:              "media",
-		Addr:                  fmt.Sprintf("%s:%d", "192.168.3.106", 5433),
+		Addr:                  fmt.Sprintf("%s:%d", "127.0.0.1", 5433),
 		RetryStatementTimeout: true,
 		MaxRetries:            4,
 		MinRetryBackoff:       250 * 6000,
@@ -130,6 +139,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			if err != nil {
 				fmt.Println("Connect Fail")
 				fmt.Println("ERR:: ", err)
+				return err
 
 			}
 			fmt.Println("Connect success")
@@ -147,13 +157,67 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	image := &media{Name: newPath, TypeFile: detectedFileType, IsPublic: false}
 	err = db.Insert(image)
 	if err != nil {
+		return err
 		fmt.Println("ERR:: ", err)
 	}
 	w.Write([]byte(fmt.Sprintf("SUCCESS:: %s", newPath)))
-
+	return nil
 }
 
+// ContextKey is used for context.Context value. The value requires a key that is not primitive type.
+type ContextKey string // can be unexported
+
+// ContextKeyRequestID is the ContextKey for RequestID
+const ContextKeyRequestID ContextKey = "requestID" // can be unexported
+
+// AttachRequestID will attach a brand new request ID to a http request
+func AssignRequestID(ctx context.Context) context.Context {
+
+	reqID := uuid.New()
+	fmt.Println("reqID:: ", reqID.String())
+	ctx2 := context.WithValue(ctx, ContextKeyRequestID, reqID.String())
+	fmt.Println("===2 reqID:: ", ctx.Value(ContextKeyRequestID))
+	fmt.Println("===2 reqID:: ", ctx2.Value(ContextKeyRequestID))
+
+	return ctx2
+}
+
+// GetRequestID will get reqID from a http request and return it as a string
+func GetRequestID(ctx context.Context) string {
+
+	reqID := ctx.Value(ContextKeyRequestID)
+	fmt.Println("GetRequestID reqID:: ", reqID)
+
+	if ret, ok := reqID.(string); ok {
+		return ret
+	}
+
+	return ""
+}
+
+//#region middlewares
+
+func reqIDMiddleware1(next func(http.ResponseWriter, *http.Request) error) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		ctx = AssignRequestID(ctx)
+		r = r.WithContext(ctx)
+		reqID := GetRequestID(ctx)
+		logger := log.WithField(LogFieldKeyRequestID, reqID)
+
+		logger.Infof("Incomming request %s %s %s", r.Method, r.RequestURI, r.RemoteAddr)
+		err := next(w, r)
+		log.Error(err)
+		log.Infof("Finished handling http req. %s", GetRequestID(ctx))
+	})
+}
+
+const LogFieldKeyRequestID = "requestID"
+
+//#endregion middlewares
+
 func main() {
-	http.HandleFunc("/", handler)
+	// http.HandleFunc("/", reqIDMiddleware1(handler))
+	http.Handle("/", reqIDMiddleware1(handler))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
